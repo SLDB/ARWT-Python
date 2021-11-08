a780823059e3e34521c8e8c1a897245a4b259cc6
SPIA R+ AWE <soporte@ldmatto.com>
2021-11-08 12:21:04 -0500

    work on emails.py and reports.py
    modify to proper work with qwik module 7, week 4. By creating auxiliar functions simplified from the
    mockups emails.py and reports.py

--- emails.py
+++ emails.py
@@ -28,6 +28,20 @@ def generate(sender, recipient, subject, body, attachment_path):
 
   return message
 
+
+def generateNoAttachment(sender, recipient, subject. body, attachment_path=None):
+  """Creates an email without an attachment."""
+  # Basic Email formatting
+  message = email.message.EmailMessage()
+  message["From"] = sender
+  message["To"] = recipient
+  message["Subject"] = subject
+  message.set_content(body)
+
+  return message
+
+
+
 def send(message):
   """Sends the message to the configured SMTP server."""
   mail_server = smtplib.SMTP('localhost')
--- reports.py
+++ reports.py
@@ -16,3 +16,11 @@ def generate(filename, title, additional_info, table_data):
   report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
   empty_line = Spacer(1,20)
   report.build([report_title, empty_line, report_info, empty_line, report_table])
+
+def generateEZ(filename, title, additional_info):
+  styles = getSampleStyleSheet()
+  report = SimpleDocTemplate(filename)
+  report_title = Paragraph(title, styles["h1"])
+  report_info = Paragraph(additional_info, styles["BodyText"])
+  empty_line = Spacer(1,20)
+  report.build([report_title, empty_line, report_info, empty_line])
